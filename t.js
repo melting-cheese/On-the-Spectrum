23946e3a8d46c429c3dbfc6fb66cd24d567b50ef
멜팅치즈 <79797313+ryf6q9yc@users.noreply.github.com>
2021-04-01 05:31:21 +0900

    1

--- t.js
+++ t.js
@@ -28,29 +28,48 @@ function color(type, abbr) {
 }
 function status(x,y) {
   switch (x) {
+    case "dm":
+      dm = dm + y - 4;
+      color('dominant', dm);
+      document.getElementById('dominant').style.width = dm+"%";
+      break;
+    case "mt":
+      mt = mt + y - 4;
+      color('master', mt);
+      document.getElementById('master').style.width = mt+"%";
+      break;
+    case "ht":
+      ht = ht + y - 4;
+      color('hunter', ht);
+      document.getElementById('hunter').style.width = ht+"%";
+      break;
     case "rg":
       rg = rg + y - 4;
       color('rigger', rg);
       document.getElementById('rigger').style.width = rg+"%";
-      document.getElementById('rigger').ariavaluenow = rg;
+      break;
+    case "sk":
+      sk = sk + y - 4;
+      color('spanker', sk);
+      document.getElementById('spanker').style.width = sk+"%";
       break;
     case "de":
       de = de + y - 4;
       color('degradee', de);
       document.getElementById('degradee').style.width = de+"%";
-      document.getElementById('degradee').ariavaluenow = de;
       break;
     case "rb":
       rb = rb + y - 4;
       color('ropebunny', rb);
       document.getElementById('ropebunny').style.width = rb+"%";
-      document.getElementById('ropebunny').ariavaluenow = rb;
       break;
     default:
       rb = 0
   }
 }
 var qnumber = 1;
+var dm = 50;
+var mt = 50;
 var rb = 50;
 var rg = 50;
 var de = 50;
